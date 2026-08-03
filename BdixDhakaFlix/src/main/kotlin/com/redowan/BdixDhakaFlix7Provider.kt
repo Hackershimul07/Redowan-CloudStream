2613ffff59dfb4791701f332614ad4f92c429ace
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-04 04:41:15 +0600

    Update BdixDhakaFlix7Provider.kt

--- BdixDhakaFlix/src/main/kotlin/com/redowan/BdixDhakaFlix7Provider.kt
+++ BdixDhakaFlix/src/main/kotlin/com/redowan/BdixDhakaFlix7Provider.kt
@@ -17,7 +17,7 @@ class BdixDhakaFlix7Provider : BdixDhakaFlix14Provider()  {
         "Foreign Language Movies/Korean Language/" to "Korean Movies",
         "Foreign Language Movies/Bangla Dubbing Movies/" to "Bangla Dubbing Movies",
         "Foreign Language Movies/Pakistani Movie/" to "Pakistani Movies",
-        "Kolkata Bangla Movies/(2022)/" to "Kolkata Bangla Movies",
+        "Kolkata Bangla Movies/" to "Kolkata Bangla Movies",
         "Foreign Language Movies/Chinese Language/" to "Chinese Movies"
     )
 }
