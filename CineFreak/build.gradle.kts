cd40b46cc0dbb9c164cb5a74b2b0283c4baafe53
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-06 15:50:06 +0600

    Update build.gradle.kts

--- CineFreak/build.gradle.kts
+++ CineFreak/build.gradle.kts
@@ -1,11 +1,11 @@
 // use an integer for version numbers
-version = 2
+version = 3
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
 
     language = "bn"
-    authors = listOf("Hackershimul07")
+    authors = listOf("Shimul_Ahmed")
 
     /**
      * Status int as one of the following:
