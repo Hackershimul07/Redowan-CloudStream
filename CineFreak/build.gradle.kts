8ad8911bff9ed08407baa4410c7fc9a24db9d889
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-06 15:26:24 +0600

    Create build.gradle.kts

---
+++ CineFreak/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove any of them.
+
+    language = "en"
+    authors = listOf("Hackershimul07")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     **/
+    status = 1 // Will be 3 if unspecified
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+    iconUrl = "https://cinefreak.nl/wp-content/uploads/2024/08/cropped-cgk-270x270.png"
+}
